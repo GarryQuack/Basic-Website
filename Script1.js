a014ba0351e0a29ab0b90a07e70c12361a7e0e27
Garry Quack <RianQuick2009@hotmail.com>
2023-08-18 00:36:22 +0100

    Add project files.

---
+++ Script1.js
@@ -0,0 +1,10 @@
+function btntog() {
+    var t = document.getElementById("photo");
+    if (t.style.display == 'block') {
+        t.style.display = 'none';
+    }
+    else {
+        t.style.display = 'block';
+    }
+}
+
