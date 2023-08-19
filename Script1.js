e3e51c8605e853bf70e8a330539d571ea7e38a21
Garry Quack <RianQuick2009@hotmail.com>
2023-08-19 22:43:59 +0100

    Really rough toggle

--- Script1.js
+++ Script1.js
@@ -1,4 +1,4 @@
-function btntog() {
+/*function btntog() {
     var t = document.getElementById("photo");
     if (t.style.display == 'block') {
         t.style.display = 'none';
@@ -6,5 +6,15 @@ function btntog() {
     else {
         t.style.display = 'block';
     }
-}
+}*/
 
+var t = document.getElementById("photo");
+
+const toggle = document.querySelector(".toggle input")
+
+toggle.addEventListener("click", () => {
+    const onOff = toggle.parentNode.querySelector(".onoff")
+    
+    onOff.textContent = toggle.checked ? "OFF" : "ON"
+    t.style.display = toggle.checked ? 'none' : 'block'
+})
